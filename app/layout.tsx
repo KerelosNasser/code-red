531b3ec29dd29346f213bbbe04116cb2eda2ae02
KerelosNasser <keronaser2030@gmail.com>
2026-04-23 05:34:30 -0700

    feat: add optimized background image to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono, Raleway } from "next/font/google"
+import Image from "next/image"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
@@ -22,7 +23,17 @@ export default function RootLayout({
       suppressHydrationWarning
       className={cn("antialiased", fontMono.variable, "font-sans", raleway.variable)}
     >
-      <body>
+      <body className="relative min-h-screen">
+        <div className="fixed inset-0 -z-10 overflow-hidden">
+          <Image
+            src="/background.jpg"
+            alt="Background"
+            fill
+            priority
+            className="object-cover object-center"
+            quality={100}
+          />
+        </div>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
